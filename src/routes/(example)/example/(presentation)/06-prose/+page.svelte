df2d03b01cb83fd0d8173547a261598868933ec5
Michael Riley <michaelriley@situsamc.com>
2023-03-08 17:43:57 -0700

    deploy

--- src/routes/(example)/example/(presentation)/06-prose/+page.svelte
+++ src/routes/(example)/example/(presentation)/06-prose/+page.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
 	import { addSteps } from '$lib/addsteps';
+	import Test from './Test.md'
 
 	let classP = 'text-blue-400';
 
 
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center bg-gray-900">
-<h1 class="mx-auto mb-12 text-center font-sans text-5xl font-black tracking-tighter text-white">
-	Prose...
-</h1>
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css" integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ" crossorigin="anonymous">
+</svelte:head>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center bg-gray-300">
 <div class="flex flex-col items-center gap-8 text-2xl md:flex-row md:gap-20">
 	<article class="prose">
+		<h1>Prose...</h1>
 		<p use:addSteps={[{ steps: '1', classes: classP }]}>
 			For years parents have espoused the health benefits of eating garlic bread with cheese to their
 			children, with the food earning such an iconic status in our culture that kids will often dress
 			up as warm, cheesy loaf for Halloween.
 		</p>
-		<!-- ... -->
+		<Test />
 	</article>
 </div>
 </div>
+
+
---
+++ src/routes/(example)/example/(presentation)/06-prose/Test.md
@@ -0,0 +1,19 @@
+## Test
+
+This is some *markdown*!
+
+$$x^2 = 34$$
+
+That is dollar sign math.
+
+Markdown test:
+
+1. A
+2. List
+
+* Bullets aren't working for some reason?
+* More bullets
+
+| Tables | Are |
+|-------|------|
+| spaced | weird |
